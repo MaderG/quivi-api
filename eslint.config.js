375c9268576b42e147694040fd368bafcbb96eb6
Mader <madergabriel2@gmail.com>
2024-07-11 12:59:16 -0300

    :wrench: chore: Add more lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,10 @@ import pluginJs from "@eslint/js";
 export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
+  {
+    files: ["src/**/*.js"],
+    rules: {
+      quotes: ["error", "single"],
+    },
+  },
 ];
