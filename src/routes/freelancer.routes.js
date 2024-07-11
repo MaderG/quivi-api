a2b069bba796980dbb0ee84b8d420075c86681ff
Mader <madergabriel2@gmail.com>
2024-07-11 13:10:06 -0300

    :sparkles: feat: Add freelancer controller route and configuration

--- src/routes/freelancer.routes.js
+++ src/routes/freelancer.routes.js
@@ -4,4 +4,12 @@ import FreelancerController from '../controller/freelancer.controller';
 const freelancerRouter = Router();
 const freelancerController = new FreelancerController();
 
-freelancerRouter.use('/api/freelncers', freelancerRouter);
+freelancerRouter.use(
+  '/api/freelancers',
+  Router()
+    .get('/', freelancerController.index)
+    .post('/create', freelancerController.create)
+    .get('/:id', freelancerController.show)
+    .put('/:id', freelancerController.update)
+    .delete('/:id', freelancerController.delete)
+);
